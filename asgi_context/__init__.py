d430413e5eb65f4e5edcac56f24f83d203840729
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2023-04-26 12:47:15 +0200

    Add headers extractor middleware, add tests

--- asgi_context/__init__.py
+++ asgi_context/__init__.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
 from collections import UserDict
-from collections.abc import Awaitable, Callable, Iterator
+from collections.abc import Awaitable, Callable, Iterable, Iterator
 from contextlib import contextmanager
 from contextvars import ContextVar
+from functools import cached_property
 from typing import TypeAlias
 
 Scope: TypeAlias = dict
@@ -9,11 +11,20 @@ Receive: TypeAlias = Callable[[None], Awaitable[dict]]
 Send: TypeAlias = Callable[[dict], Awaitable[None]]
 ASGIApp: TypeAlias = Callable[[Scope, Receive, Send], Awaitable[None]]
 
+HeaderName: TypeAlias = str
+HeaderValue: TypeAlias = str
 
-_http_request_context: ContextVar[dict] = ContextVar("ctx", default=dict())
+Validator: TypeAlias = Callable[[HeaderValue], bool]
+
+
+_http_request_context: ContextVar[dict] = ContextVar("ctx")
 
 
 class RequestContextException(Exception):
+    pass
+
+
+class HeaderValidationException(Exception):
     pass
 
 
@@ -25,10 +36,12 @@ class Context(UserDict):
     def data(self) -> dict:  # type: ignore[override]
         try:
             return _http_request_context.get()
-        except LookupError:
+        except LookupError as e:
             raise RequestContextException(
-                "No request context available - make sure you are using the ContextMiddleware"
-            )
+                "No request context available - make sure you are using the ContextMiddleware. "
+                "In case you're using Starlette based framework and using add_middleware method "
+                "make sure to call it after any middleware that uses http_request_context."
+            ) from e
 
 
 @contextmanager
@@ -40,9 +53,6 @@ def new_context() -> Iterator[None]:
         _http_request_context.reset(token)
 
 
-http_request_context = Context()
-
-
 class ContextMiddleware:
     __slots__ = ("app",)
 
@@ -50,11 +60,80 @@ class ContextMiddleware:
         self.app = app
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if not scope["type"] == "http":
-            return await self.app(scope, receive, send)
+        assert "type" in scope, "ASGI scope must contain a 'type' key"
 
-        with new_context():
+        if not scope["type"] == "http":
+            await self.app(scope, receive, send)
+        else:
+            with new_context():
+                await self.app(scope, receive, send)
+
+
+class AbstractExtractHeadersMiddleware(ABC):
+    __slots__ = ("app",)
+
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+
+    @property
+    @abstractmethod
+    def header_names(self) -> Iterable[HeaderName]:
+        pass
+
+    @property
+    @abstractmethod
+    def validators(self) -> dict[HeaderName, Validator]:
+        pass
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        assert "type" in scope, "ASGI scope must contain a 'type' key"
+
+        if not scope["type"] == "http":
+            await self.app(scope, receive, send)
+        else:
+            headers = {name.decode().lower(): value.decode() for name, value in scope["headers"]}
+
+            for name in self.header_names:
+                header_value = headers.get(name.lower())
+
+                if (validate := self.validators.get(name)) and header_value:
+                    if not validate(header_value):
+                        raise HeaderValidationException(f"Header {name} failed validation")
+
+                http_request_context[name] = header_value
+
             await self.app(scope, receive, send)
 
 
-__all__ = ("http_request_context", "ContextMiddleware")
+class ExtractHeadersMiddlewareFactory:
+    @staticmethod
+    def build(
+        base_name: str,
+        header_names: Iterable[HeaderName],
+        validators: dict[HeaderName, Validator] | None = None,
+    ) -> type[AbstractExtractHeadersMiddleware]:
+        name_parts = base_name.lower().replace(" ", "_").split("_")
+        name = "".join(name_part.capitalize() for name_part in name_parts)
+        header_names_property = lambda self: tuple(header_names)
+        validators_property = lambda self: validators or dict()
+
+        return type(
+            f"{name}ExtractHeadersMiddleware",
+            (AbstractExtractHeadersMiddleware,),
+            {
+                "header_names": cached_property(header_names_property),
+                "validators": cached_property(validators_property),
+            },
+        )
+
+
+http_request_context = Context()
+
+
+__all__ = (
+    "http_request_context",
+    "ContextMiddleware",
+    "ExtractHeadersMiddlewareFactory",
+    "RequestContextException",
+    "HeaderValidationException",
+)
